f1fde045e78e6463fa317a1e186acc3476db8c54
Cory Mawhorter <cory.mawhorter@gmail.com>
2014-12-22 23:20:31 -0800

    developing content format

--- src/cli/lib/server/router.js
+++ src/cli/lib/server/router.js
@@ -2,9 +2,68 @@ var fs = require('fs')
   , path = require('path');
 
 var express = require('express')
-  , yaml = require('js-yaml');
+  , yaml = require('js-yaml')
+  , cheerio = require('cheerio');
 
-var db, fancy, models, site;
+var db, fancy, models, site, $;
+
+// TODO: passed in from fancy
+models = {};
+
+
+[
+    './www/data/content/article1.html'
+  , './www/data/content/article2.html'
+  , './www/data/content/article3.html'
+].forEach(function(f) {
+  $ = cheerio.load(fs.readFileSync(path.join(process.cwd(), f)));
+
+  var props = {}
+    , output = [];
+
+  $('meta').each(function() {
+    var $el = $(this)
+      , name = $el.attr('name')
+      , val = $el.attr('content') || '';
+    if (name) {
+      if (props[name]) {
+        if ('object' !== typeof props[name] || !('length' in props[name])) {
+          props[name] = [ props[name] ];
+        }
+        props[name].push(val);
+      }
+      else {
+        props[$el.attr('name')] = $el.attr('content') || '';
+      }
+    }
+  });
+
+  props.title = $('title').text() || '';
+  props.contentType = 'text/html; charset=utf-8';
+  $('meta[http-equiv][content]').each(function() {
+    var $this = $(this);
+    if ($this.attr('http-equiv').toLowerCase() === 'content-type') {
+      props.contentType = $this.attr('content');
+    }
+  });
+
+  // $('link[rel="alternate"]').each(function() {
+  //   var $el = $(this);
+  //   output.push({
+  //       type: ($el.attr('type') || '').toLowerCase()
+  //     , title: ($el.attr('title') || '').toLowerCase()
+  //     , href: ($el.attr('href') || '').toLowerCase()
+  //   });
+  // });
+
+  props.body = $('body').html();
+
+  var model = $('meta[name="model"]').attr('content');
+  if (model) {
+    if (!models[model]) models[model] = {};
+    models[model][props.url] = props;
+  }
+});
 
 // var db = require(path.join(process.cwd(), './.fancy/db/'));
 
@@ -26,46 +85,45 @@ fancy = {
     else {
       callback();
     }
+  },
+
+  forEach: function(obj, callback) {
+    for (var k in obj) {
+      callback(obj[k], k);
+    }
   }
 }
 
+// var parsley = require('parsley');
+// parsley(fs.createReadStream(path.join(process.cwd(), './www/data/content/article1.html')), function (req) {
+//   var head = [];
+//   req.on('rawHead', function (buf) {
+//     head.push(buf);
+//   });
 
-// TODO: passed in from fancy
-models = {
-  articles: []
-};
+//   var body = [];
+//   req.on('rawBody', function (buf) {
+//     body.push(buf);
+//   });
 
+//   req.on('error', function(err) {
+//     throw err;
+//   });
 
-var parsley = require('parsley');
-parsley(fs.createReadStream(path.join(process.cwd(), './www/data/content/article1.html')), function (req) {
-  var head = [];
-  req.on('rawHead', function (buf) {
-    head.push(buf);
-  });
+//   req.on('end', function () {
+//     console.dir(head.map(String));
+//     console.dir(body.map(String));
+//     models.articles.push({
+//         title: head['x-fancy-model-title']
+//       , postedDate: head['x-fancy-model-posteddate']
+//       , author: head['x-fancy-model-author']
+//       , body: body
+//     });
 
-  var body = [];
-  req.on('rawBody', function (buf) {
-    body.push(buf);
-  });
-
-  req.on('error', function(err) {
-    throw err;
-  });
-
-  req.on('end', function () {
-    console.dir(head.map(String));
-    console.dir(body.map(String));
-    models.articles.push({
-        title: head['x-fancy-model-title']
-      , postedDate: head['x-fancy-model-posteddate']
-      , author: head['x-fancy-model-author']
-      , body: body
-    });
-
-    console.log(models.articles);
-    process.exit();
-  });
-});
+//     console.log(models.articles);
+//     process.exit();
+//   });
+// });
 
 var router = express.Router();
 router.get('*', function(req, res) {
