8fa769626b96dbccfcc25507809ee6fd40e24c01
cmawhorter <cory.mawhorter@gmail.com>
2014-12-26 21:29:46 -0800

    move plugin outside

--- src/cli/lib/server/router.js
+++ src/cli/lib/server/router.js
@@ -246,42 +246,7 @@ module.exports = function(workingDir) {
       , site: site
       , page: page || {}
       , plugins: {
-          pagination: function(collection, options, callback) {
-            console.log('Pagination plugin');
-            var pageStart = parseInt(options.pageStart, 10) || 0;
-            var pageLimit = parseInt(options.pageLimit, 10) || 10;
-
-            var start = pageStart * pageLimit;
-            var stop = start + pageLimit;
-
-            // console.log('collection len', collection.length, 'pageStart', pageStart, 'pageLimit', pageLimit);
-
-            var getColKey = 'length' in collection ? function(index) { return index; } : function(index) { return Object.keys(collection)[index]; };
-            var colLen = 'length' in collection ? collection.length : Object.keys(collection).length;
-
-            for (var i=start; i < colLen && i < stop; i++) {
-              var item = collection[getColKey(i)];
-              // console.log('iterating', item);
-              callback(item);
-            }
-
-            var previous = pageStart - 1
-              , next = pageStart + 1;
-
-            if (next >= Math.ceil(colLen / pageLimit)) {
-              next = null;
-            }
-
-            if (previous < 1) {
-              previous = null;
-            }
-
-            return {
-                current: pageStart
-              , previous: previous
-              , next: next
-            };
-          }
+          pagination: require('./pagination')
         }
       , moment: moment
     });
